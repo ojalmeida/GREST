43367d0509cfbd4baaf7565dfac79aff989c24de
farinap5 <farinap5@protonmail.com>
2022-01-14 18:20:20 -0300

    config behaviors

--- src/server/handler.go
+++ src/server/handler.go
@@ -354,83 +354,256 @@ func GetHandler(behavior db.Behavior) func(writer http.ResponseWriter, request *
 
 func GetConfigHandler(endpoint string) func(writer http.ResponseWriter, request *http.Request) {
 
+	driverName := "sqlite3-config"
+
 	switch endpoint {
 
-	//case "/config/behaviors":
-	//
-	//	var needReload float32 = 0
-	//
-	//	return func(writer http.ResponseWriter, request *http.Request) {
-	//
-	//		switch request.Method {
-	//
-	//		case http.MethodGet:
-	//
-	//		case http.MethodPost: // If Method is Post.
-	//			var res Response
-	//			var requestPayload GetPayload
-	//			var responseStatus = http.StatusOK
-	//			var errors []string
-	//
-	//			writer.Header().Set("Content-Type", "application/json")
-	//
-	//			rawReqPayload, err := ioutil.ReadAll(request.Body)
-	//
-	//			log.Println(fmt.Sprintf("Payload: %s", rawReqPayload))
-	//
-	//			if err != nil {
-	//				errors = append(errors, "Impossible to read body")
-	//				responseStatus = http.StatusInternalServerError
-	//			} else {
-	//
-	//				if isValidPayload("POST", string(rawReqPayload)) {
-	//
-	//					_ = json.Unmarshal(rawReqPayload, &GetPayload{})
-	//
-	//					if unknownKeys == nil {
-	//
-	//						err = db.Create(behavior.PathMapping.Table, fixedData)
-	//
-	//						if err != nil {
-	//							errors = append(errors, err.Error())
-	//							responseStatus = http.StatusInternalServerError
-	//						} else {
-	//							needReload += float32(0.5)
-	//						}
-	//
-	//					} else {
-	//
-	//						for i := range unknownKeys {
-	//
-	//							errors = append(errors, fmt.Sprintf("Invalid criteria: %s", unknownKeys[i]))
-	//
-	//						}
-	//
-	//					}
-	//
-	//				} else {
-	//
-	//					errors = append(errors, "Invalid payload")
-	//					responseStatus = http.StatusBadRequest
-	//				}
-	//
-	//			}
-	//
-	//		case http.MethodPut:
-	//
-	//		case http.MethodDelete:
-	//
-	//		case http.MethodOptions:
-	//
-	//		case http.MethodHead:
-	//
-	//		}
-	//
-	//		if needReload == 1 {
-	//			defer ReloadConfigServer()
-	//		}
-	//
-	//	}
+	case "/config/behaviors":
+
+		var needReload float32 = 0
+		tableName := ""
+
+		return func(writer http.ResponseWriter, request *http.Request) {
+
+			switch request.Method {
+
+			case http.MethodGet:
+				var res Response
+				var responseData []map[string]string
+				var requestPayload GetPayload
+				var responseStatus = http.StatusOK
+				var errors []string
+
+				if isValidQueryString(request.URL.String()) {
+
+					requestPayload = toGetPayload(request.URL.RawQuery)
+
+					var err error
+
+					responseData, err = db.Read(tableName, requestPayload.Must, driverName)
+
+					if err != nil {
+						errors = append(errors, err.Error())
+					}
+
+				} else {
+
+					errors = append(errors, "Invalid payload")
+					responseStatus = http.StatusBadRequest
+				}
+
+				res.Response = responseData
+				res.Status = responseStatus
+				res.Errors = errors
+
+				response, _ := json.Marshal(res)
+
+				writer.WriteHeader(responseStatus)
+				_, _ = writer.Write(response)
+
+				log.Println(fmt.Sprintf("Response status: %d", res.Status))
+				log.Println(fmt.Sprintf("Errors: %s", res.Errors))
+
+			case http.MethodPost: // If Method is Post.
+				var res Response
+				var requestPayload PostPayload
+				var responseStatus = http.StatusOK
+				var errors []string
+
+				writer.Header().Set("Content-Type", "application/json")
+
+				rawReqPayload, err := ioutil.ReadAll(request.Body)
+
+				log.Println(fmt.Sprintf("Payload: %s", rawReqPayload))
+
+				if err != nil {
+					errors = append(errors, "Impossible to read body")
+					responseStatus = http.StatusInternalServerError
+				} else {
+
+					if isValidPayload("POST", string(rawReqPayload)) {
+
+						_ = json.Unmarshal(rawReqPayload, &requestPayload)
+
+						err = db.Create(tableName, requestPayload.Set, driverName)
+
+						if err != nil {
+							errors = append(errors, err.Error())
+							responseStatus = http.StatusInternalServerError
+						}
+
+					} else {
+
+						errors = append(errors, "Invalid payload")
+						responseStatus = http.StatusBadRequest
+					}
+
+				}
+
+				res.Response = nil
+				res.Status = responseStatus
+				res.Errors = errors
+
+				response, err := json.Marshal(res)
+
+				writer.WriteHeader(res.Status)
+				_, _ = writer.Write(response)
+
+				log.Println(fmt.Sprintf("Response status: %d", res.Status))
+				log.Println(fmt.Sprintf("Errors: %s", res.Errors))
+
+			case http.MethodPut: // If Method is Put
+				var res Response
+				var requestPayload PutPayload
+				var responseStatus = http.StatusOK
+				var errors []string
+
+				rawReqPayload, err := ioutil.ReadAll(request.Body)
+
+				log.Println(fmt.Sprintf("Payload: %s", rawReqPayload))
+
+				if err != nil {
+					errors = append(errors, "Impossible to read body")
+					responseStatus = http.StatusInternalServerError
+				} else {
+
+					if isValidPayload("PUT", string(rawReqPayload)) {
+
+						_ = json.Unmarshal(rawReqPayload, &requestPayload)
+
+						err = db.Update(tableName, requestPayload.Must, requestPayload.Set, driverName)
+
+						if err != nil {
+							errors = append(errors, err.Error())
+							responseStatus = http.StatusInternalServerError
+						}
+
+					} else {
+
+						errors = append(errors, "Invalid payload")
+						responseStatus = http.StatusBadRequest
+					}
+
+				}
+
+				var correctedResponse []map[string]string
+
+				res.Response = correctedResponse
+				res.Status = responseStatus
+				res.Errors = errors
+
+				response, err := json.Marshal(res)
+
+				writer.WriteHeader(res.Status)
+				_, _ = writer.Write(response)
+
+				log.Println(fmt.Sprintf("Response status: %d", res.Status))
+				log.Println(fmt.Sprintf("Errors: %s", res.Errors))
+
+			case http.MethodDelete:
+				var res Response
+				var requestPayload DeletePayload
+				var responseStatus = http.StatusOK
+				var errors []string
+
+				rawReqPayload, err := ioutil.ReadAll(request.Body)
+
+				log.Println(fmt.Sprintf("Payload: %s", rawReqPayload))
+
+				if err != nil {
+					errors = append(errors, "Impossible to read body")
+					responseStatus = http.StatusInternalServerError
+				} else {
+
+					if isValidPayload("DELETE", string(rawReqPayload)) {
+
+						_ = json.Unmarshal(rawReqPayload, &requestPayload)
+
+						err = db.Delete(tableName, requestPayload.Must, driverName)
+
+						if err != nil {
+							errors = append(errors, err.Error())
+							responseStatus = http.StatusInternalServerError
+						}
+
+					} else {
+
+						errors = append(errors, "Invalid payload")
+						responseStatus = http.StatusBadRequest
+					}
+
+				}
+
+				var correctedResponse []map[string]string
+
+				res.Response = correctedResponse
+				res.Status = responseStatus
+				res.Errors = errors
+
+				response, err := json.Marshal(res)
+				writer.WriteHeader(res.Status)
+				_, _ = writer.Write(response)
+
+				log.Println(fmt.Sprintf("Response status: %d", res.Status))
+				log.Println(fmt.Sprintf("Errors: %s", res.Errors))
+			case http.MethodOptions:
+
+			case http.MethodHead:
+				var res Response
+				var requestPayload GetPayload
+				var responseStatus = http.StatusOK
+				var errors []string
+
+				rawReqPayload, err := ioutil.ReadAll(request.Body)
+
+				log.Println(fmt.Sprintf("Payload: %s", rawReqPayload))
+
+				if err != nil {
+					errors = append(errors, "Impossible to read body")
+					responseStatus = http.StatusInternalServerError
+				} else {
+
+					if isValidPayload("GET", string(rawReqPayload)) {
+
+						_ = json.Unmarshal(rawReqPayload, &requestPayload)
+
+						_, err = db.Read(tableName, requestPayload.Must, driverName)
+
+						if err != nil {
+							errors = append(errors, err.Error())
+						}
+
+					} else {
+
+						errors = append(errors, "Invalid payload")
+						responseStatus = http.StatusBadRequest
+					}
+
+				}
+
+				res.Status = responseStatus
+
+				writer.WriteHeader(res.Status)
+
+				log.Println(fmt.Sprintf("Response status: %d", res.Status))
+				log.Println(fmt.Sprintf("Errors: %s", res.Errors))
+
+			}
+
+			if needReload == 1 {
+				defer ReloadConfigServer()
+			}
+
+		}
+
+
+	}
+
+			if needReload == 1 {
+				defer ReloadConfigServer()
+			}
+
+		}
 
 	case "/config/path-mappings":
 
