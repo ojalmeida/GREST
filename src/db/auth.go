225980459a0380c25e983e5fe5d30e6350d60a03
pietro <pietro@go.go>
2022-02-20 21:05:15 -0300

    new auth funcs

--- src/db/auth.go
+++ src/db/auth.go
@@ -12,8 +12,6 @@ import (
 	The function returns a string with the Username and bool value
 	which references to whether the Token is from valid user or not.
 */
-
-// maybe ValidateToken() ?
 func TokenValidation(r *http.Request) Profile {
 	var profile Profile
 	Token := r.Header.Get("GToken")
@@ -42,6 +40,10 @@ func TokenValidation(r *http.Request) Profile {
 	return profile
 }
 
+/*
+	CreateUser creates a new user from the passed profile.
+	Username and Passkey are needed.
+*/
 func CreateUser(profile Profile) error {
 	transaction, err := LocalConn.Begin()
 	if err != nil {
@@ -55,7 +57,7 @@ func CreateUser(profile Profile) error {
 	}
 
 	if profile.Token == "" {
-		profile.Token = Randomstring(64)
+		profile.Token = GenerateToken(64)
 	}
 
 	_, err = stt.Exec(profile.Username, profile.Password, 0, profile.Token)
@@ -71,36 +73,34 @@ func CreateUser(profile Profile) error {
 }
 
 /*
-	Permission handle user permissions read/write/edit
-	Parameters: Profile and action. Return error.
-	Actions:
-	set - Set/change permission.
-	get - Return permission.
+	GetPermission returns the permission from the wanted user.
+	Token is needed.
 */
-func Permission(profile Profile, action string) (int, error) {
-	switch action {
-	case "set":
-		transaction, err := LocalConn.Begin()
-		if err != nil {
-			return 0, err
-		}
-		_, err = transaction.Exec("UPDATE profile SET perm = ? WHERE username = ?;", profile.Perm, profile.Username)
-		if err != nil {
-			return 0, err
-		}
-	case "get":
-		row := LocalConn.QueryRow("SELECT perm FROM profile WHERE username = ?;", profile.Username)
-		var perm int
-		err := row.Scan(&perm)
-		if err != nil {
-			return 0, err
-		} else {
-			return perm, nil
-		}
-	default:
-
+func GetPermission(profile Profile) (int, error) {
+	row := LocalConn.QueryRow("SELECT perm FROM profile WHERE token = ?;", profile.Token)
+	var perm int
+	err := row.Scan(&perm)
+	if err != nil {
+		return 0, err
+	} else {
+		return perm, nil
 	}
-	return 0, nil
+}
+
+/*
+	SetPermission is a func that has the ability to set up
+	and change the permissions from a user.
+*/
+func SetPermission(profile Profile) error {
+	transaction, err := LocalConn.Begin()
+	if err != nil {
+		return err
+	}
+	_, err = transaction.Exec("UPDATE profile SET perm = ? WHERE username = ?;", profile.Perm, profile.Username)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 /*
@@ -114,8 +114,39 @@ func DropUser(p Profile) error {
 	return nil
 }
 
-// maybe GenerateToken() ?
-func Randomstring(length int) string {
+// TODO: Use hash function for authentication!!!!
+
+/*
+	GetToken seems to be a "login" function.
+	It receives the credentials and returns the token.
+*/
+func GetToken(profile Profile) (string, error) {
+	row := LocalConn.QueryRow("SELECT token FROM profile WHERE username = ? AND passkey = ?;", profile.Username, profile.Password)
+	var token string
+	err := row.Scan(&token)
+	if err != nil {
+		return "", err
+	} else {
+		return token, nil
+	}
+}
+
+func RecreateToken(profile Profile) (string, error) {
+	transaction, err := LocalConn.Begin()
+	if err != nil {
+		return "", err
+	}
+	_, err = transaction.Exec("UPDATE profile SET token = ? WHERE token = ?;", GenerateToken(64), profile.Perm)
+	if err != nil {
+		return "", err
+	}
+	return "OK", nil
+}
+
+/*
+	GenerateToken fuction creates a new API token with random values
+*/
+func GenerateToken(length int) string {
 	s1 := rand.NewSource(time.Now().UnixNano())
 	r1 := rand.New(s1)
 	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
@@ -126,5 +157,6 @@ func Randomstring(length int) string {
 	return string(b)
 }
 
-// TODO: uniform function names
-// TODO: delegate just only thing to each function (maybe SetPermission and GetPermission instead of switch case ?) -> Clean Code chapter 3, by Robert C. Martin (https://cloudflare-ipfs.com/ipfs/bafykbzacedgzwm4qwdxqkq5oy6fxgwebffgxcncjaqkhxhhuawvbrmcdxe2u2?filename=Robert%20C.%20Martin%20-%20Clean%20Code_%20A%20Handbook%20of%20Agile%20Software%20Craftsmanship-Prentice%20Hall%20%282008%29.pdf)
+// TODO: [ ] Use hash function for authentication!!!!
+// DONE: [X] uniform function names
+// DONE: [X] delegate just only thing to each function (maybe SetPermission and GetPermission instead of switch case ?) -> Clean Code chapter 3, by Robert C. Martin (https://cloudflare-ipfs.com/ipfs/bafykbzacedgzwm4qwdxqkq5oy6fxgwebffgxcncjaqkhxhhuawvbrmcdxe2u2?filename=Robert%20C.%20Martin%20-%20Clean%20Code_%20A%20Handbook%20of%20Agile%20Software%20Craftsmanship-Prentice%20Hall%20%282008%29.pdf)
